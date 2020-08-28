eb60a62f9957841b14fc76d1bbca377e834f96e9
Niki_068 <nikitasingh7698@gmail.com>
2020-08-28 18:52:09 +0530

    database switched to sqllite

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,18 +79,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': 'Blogging',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
+    #  'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'django',
+    #     'USER': 'django',
+    #     'PASSWORD': 'Blogging',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
+    }
 }
 
 
